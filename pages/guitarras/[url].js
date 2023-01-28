bc44bc637e9722814767a4750a28aa3e3a7e42f8
Mateo Rodriguez <luismateor73@gmail.com>
2023-01-28 15:22:35 -0500

    Ruta dinámica para cada guitarra

---
+++ pages/guitarras/[url].js
@@ -0,0 +1,96 @@
+import { use, useState } from "react"
+import Image from "next/image"
+import styles from '../../styles/guitarras.module.css'
+import Layout from "@/components/layout"
+import stylesForm from "../../styles/formulario.module.css"
+
+export default function Producto({ guitarra, agregarCarrito }){
+
+  const [ cantidad, setCantidad ] = useState(0)
+  const { nombre, descripcion, imagen, precio } = guitarra[0].attributes
+
+  const handleSubmit = e =>{
+    e.preventDefault()
+
+    if(cantidad < 1){
+      alert('Cantidad no válida')
+      return
+    }
+
+    //Construir objeto
+    const guitarraSeleccionada = {
+      id:guitarra[0].id,
+      imagen: imagen.data.attributes.url,
+      nombre,
+      precio,
+      cantidad
+    }
+
+    //Pasando esa info al Context
+    agregarCarrito(guitarraSeleccionada)
+  }
+
+  return (
+    <Layout
+      title={`${nombre}`}
+      description={`Tienda de Guitarra, comprar guitarra ${nombre}`}
+    >
+      <div className={styles.guitarra}>
+        <Image src={imagen.data.attributes.url} width={600} height={400} alt={`Imagen de Guitarra ${nombre}}`} />
+
+        <div className={styles.contenido}>
+          <h3>{nombre}</h3>
+          <p className={styles.descripcion}>{descripcion}</p>
+          <p className={styles.precio}>${precio}</p>
+
+          <form
+            className={stylesForm.formulario}
+            onSubmit={handleSubmit}
+          >
+              <label htmlFor="cantidad">Cantidad</label>
+
+              <select
+                id="cantidad"
+                onChange={e => setCantidad(parseInt(e.target.value))}
+              >
+                <option value="0">-- Seleccione --</option>
+                <option value="1">1</option>
+                <option value="2">2</option>
+                <option value="3">3</option>
+                <option value="4">4</option>
+                <option value="5">5</option>
+              </select>
+
+            <input type="submit" value="Agregar al Carrito"/>
+          </form>
+        </div>
+      </div>
+    </Layout>
+  )
+}
+
+export async function getStaticPaths(){
+  const respuesta = await fetch(`${process.env.API_URL}/guitarras`)
+  const { data }= await respuesta.json()
+
+  const paths = data.map(guitarra => ({
+    params:{
+      url: guitarra.attributes.url
+    }
+  }))
+
+  return{
+    paths,
+    fallback: false
+  }
+}
+
+export async function  getStaticProps({params: {url}}){
+    const respuesta = await fetch(`${process.env.API_URL}/guitarras?filters[url]=${url}&populate=imagen`)
+    const { data: guitarra } = await respuesta.json()
+    return{
+      props:{
+        guitarra
+      }
+    }
+}
